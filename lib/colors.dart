a2d340c8a37d2023775a61af939258e3b82f49f6
Diksha Rawat <30834418+Diksha0125@users.noreply.github.com>
2021-05-01 01:25:30 +0530

    Delete lib directory

--- lib/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xff0082DC);
-const lightGrey = Color(0xFFECEFF1);
-const grey = Color(0xFF90A4AE);
-const black = Color(0xff000000);
-const white = Color(0xFFFFFFFF);
-const blueZodiac = Color(0xff0B1033);
